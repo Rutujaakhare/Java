33795d6292a092a6ac1d7ca16c9486e0d364c096
Rutuja <rutujaakhare2@gmail.com>
2023-03-29 13:23:01 +0530

    operators example added

---
+++ src/com/java/OperandExample.java
@@ -0,0 +1,41 @@
+package com.java;
+
+public class OperandExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20;
+		int result=(a<b)?a:b;
+		System.out.println(result);
+		
+		System.out.println(a+b);
+
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a!=b);
+		System.out.println(a==b);
+		
+		System.out.println(a>>2);
+		System.out.println(b<<2);
+		
+		a=a+10;
+		a+=10;
+		
+		a=a*b;
+		a*=b;
+
+		
+		
+
+
+
+	}
+
+}
